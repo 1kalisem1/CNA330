1eec8be9df6e0659b4e6ebc771f93dac714ed0c0
Semion Kalinin <50159960+1kalisem1@users.noreply.github.com>
2019-11-05 19:03:39 -0800

    commited 11/5/19 SK

--- JobHunter/JobHunter.py
+++ JobHunter/JobHunter.py
@@ -1,6 +1,8 @@
 # This script pulls from a job website and stores positions into a database. If there is a new posting it notifies the user.
 # CNA 330
-# Zachary Rubin, zrubin@rtc.edu
+# Semion Kalinin sskalinin@student.rtc.edu
+#Alan, Brandon, Emily and Sam helped me with the project
+
 import mysql.connector
 import sys
 import json
@@ -19,32 +21,38 @@ def connect_to_sql():
 # Create the table structure
 def create_tables(cursor, table):
     ## Add your code here. Starter code below
-    cursor.execute('''CREATE TABLE IF NOT EXISTS tablename (id INT PRIMARY KEY); ''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Table jobhunter {
+ ID INTEGER PRIMARY KEY AUTOINCREMENT,
+ Type varchar
+ Title varchar
+ Description text
+ Job_id varchar
+ Created_at DATE
+ Company varchar
+ Location varchar
+ How_to_apply varchar
+}
     return
-
 # Query the database.
 # You should not need to edit anything in this function
 def query_sql(cursor, query):
     cursor.execute(query)
     return cursor
-
 # Add a new job
 def add_new_job(cursor, jobdetails):
     ## Add your code here
-    query = "INSERT INTO"
+    query = "INSERT INTO jobhunter (ID, Type, Title,Description, Job_id, Created_at, Company, Location, How-to_apply ) VALUES (1, "IT", "system admin", "any", "Oct 23 2019", "any", "Everett", "Online app")
     return query_sql(cursor, query)
-
 # Check if new job
 def check_if_job_exists(cursor, jobdetails):
     ## Add your code here
-    query = "SELECT"
+    query = "SELECT FROM WHERE = ("ID", "Type", Title", "Job_id") 
     return query_sql(cursor, query)
-
 def delete_job(cursor, jobdetails):
     ## Add your code here
+    Job_id = get_job_id(cursor, jobdetails)
     query = "UPDATE"
     return query_sql(cursor, query)
-
 # Grab new jobs from a website
 def fetch_new_jobs(arg_dict):
     # Code from https://github.com/RTCedu/CNA336/blob/master/Spring2018/Sql.py
@@ -57,7 +65,6 @@ def fetch_new_jobs(arg_dict):
     except:
         pass
     return jsonpage
-
 # Load a text-based configuration file
 def load_config_file(filename):
     argument_dictionary = 0
@@ -73,23 +80,21 @@ def load_config_file(filename):
         file = open(filename, "w")
         file.write("")
         file.close()
-
     ## Add in information for argument dictionary
     return argument_dictionary
-
+    
+    for row in How_to_apply:
+    argument_dictionary += row
+    return argument_dictionary 
 # Main area of the code.
 def jobhunt(arg_dict):
     # Fetch jobs from website
     jobpage = fetch_new_jobs(arg_dict)
     # print (jobpage)
-    ## Add your code here to parse the job page
-
-    ## Add in your code here to check if the job already exists in the DB
-
-    ## Add in your code here to notify the user of a new posting
-
-    ## EXTRA CREDIT: Add your code to delete old entries
-
+    for job in jobpage:
+    if check_if_job_exists(cursor, job):
+    return jobpage
+   
 # Setup portion of the program. Take arguments and set up the script
 # You should not need to edit anything here.
 def main():
@@ -98,10 +103,9 @@ def main():
     cursor = conn.cursor()
     create_tables(cursor, "table")
     # Load text file and store arguments into dictionary
-    arg_dict = load_config_file(sys.argv[1])
+    arg_dict = 0 (sys.argv[1])
     while(1):
         jobhunt(arg_dict)
         time.sleep(3600) # Sleep for 1h
-
-if __name__ == '__main__':
-    main()
+    if __name__ == '__main__':
+        main()
